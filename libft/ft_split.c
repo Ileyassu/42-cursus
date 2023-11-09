5b7b694edacc4d215657a322cba2f20d9231ed64
Ileyassu <98291291+Ileyassu@users.noreply.github.com>
2023-11-09 13:53:51 +0100

    Update ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,104 +5,111 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibenaiss <ibenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/04 18:42:47 by ibenaiss          #+#    #+#             */
-/*   Updated: 2023/11/04 18:49:57 by ibenaiss         ###   ########.fr       */
+/*   Created: 2023/11/09 13:38:54 by ibenaiss          #+#    #+#             */
+/*   Updated: 2023/11/09 13:52:56 by ibenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int wordLen(const char *s, char c)
+static int	wordlength(const char *s, char c)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    while(*s && *s++ != c)
-    {
-        count++;
-    }
-    return (count);
+	count = 0;
+	while (*s && *s++ != c)
+	{
+		count++;
+	}
+	return (count);
 }
 
-static int countWords(const char *s, char c)
+static int	countwords(const char *s, char c)
 {
-    int isWord;
-    int count;
+	int	isword;
+	int	count;
 
-    count = 0;
-    isWord = 0;
-    while(*s)
-    {
-        if(*s != c && isWord)
-        {
-            isWord = 0;
-            count++;
-        }
-        else if(*s == c)
-        {
-            isWord = 1;
-        }
-        s++;
-    }
-    return (count);
+	count = 0;
+	isword = 1;
+	while (*s)
+	{
+		if (*s != c && isword)
+		{
+			isword = 0;
+			count++;
+		}
+		else if (*s == c)
+		{
+			isword = 1;
+		}
+		s++;
+	}
+	return (count);
 }
-static char *ft_strndup(const char *str, int size)
+
+static char	*ft_strndup(const char *str, int size)
 {
-    char *ptr;
-    int i;
+	char	*ptr;
+	int		i;
 
-    i = 0;
-    ptr = (char *)malloc(sizeof(char) * size + 1);
-    
-    if(ptr != NULL)
-    {
-        while(str[i] && i < size)
-        {
-            ptr[i] = str[i];
-            i++;
-        }
-        ptr[i] = '\0';
-        return (ptr);
-    }
-    return (NULL);
+	i = 0;
+	ptr = (char *)malloc(sizeof(char) * size + 1);
+	if (ptr != NULL)
+	{
+		while (str[i] && i < size)
+		{
+			ptr[i] = str[i];
+			i++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
+	return (NULL);
 }
+
 static void	ft_free_arr(char **s, int i)
 {
 	while (i--)
 		free(s[i]);
 	free(s);
 }
-char **ft_split(const char *s, char c)
+
+char	**ft_split(const char *s, char c)
 {
-    char **ptr;
-    int count;
-    int wordlen;
-    int i;
+	char	**ptr;
+	int		count;
+	int		wordlen;
+	int		i;
 
-    i = 0;
-    wordlen = 0;
-    count = countWords(s, c);
-    ptr = (char **)malloc(sizeof(char *) * count + 1);
-    if(ptr == NULL)
+	i = 0;
+	wordlen = 0;
+	count = countwords(s, c);
+	ptr = (char **)malloc(sizeof(char *) * count + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < count)
+	{
+		while (*s && *s == c)
+			s++;
+		wordlen = wordlength((char *)s, c);
+		ptr[i] = ft_strndup((char *)s, wordlen);
+		if (ptr[i] == NULL)
+		{
+			ft_free_arr(ptr, i);
+			return (NULL);
+		}
+		s += wordlen;
+		i++;
+	}
+	ptr[count] = NULL;
+	return (ptr);
+}
+int main()
+{
+    int i = 0;
+    char **ptr = ft_split("qqq ww  ww e  ewew ewe", ' ');
+    while(ptr[i])
     {
-        ft_free_arr(ptr, i);
-        return NULL;
+       printf("%s\n", ptr[i++]); 
     }
-    while(i < count)
-    {
-        while(*s && *s == c)
-            s++;
-
-        wordlen = wordLen((char *)s, c);
-        ptr[i] = ft_strndup((char *)s, wordlen);
-        if(ptr == NULL)
-        {
-            ft_free_arr(ptr, i);
-            return NULL;
-        }
-        s += wordlen;
-        i++;
-    }
-    ptr[count] = NULL;
-    return ptr;
 }
