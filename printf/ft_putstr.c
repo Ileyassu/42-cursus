eab587890cd9eeac007ddcd3183eb9cee0e97335
Ileyassu <ilyas.benaissa@um5r.ac.ma>
2023-11-28 00:57:29 +0100

    added putstr protection

--- printf/ft_putstr.c
+++ printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: ilyas <ilyas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 20:28:12 by ilyas             #+#    #+#             */
-/*   Updated: 2023/11/27 20:28:13 by ilyas            ###   ########.fr       */
+/*   Updated: 2023/11/28 00:55:33 by ilyas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 int ft_putstr(char *s)
 {
+    if(s == NULL)
+    {
+        write(1, "(null)", 7);
+        return (0);
+    }
     int i;
     int count;
-
+    
     i = 0;
     count = ft_strlen(s);
     while(s[i])
